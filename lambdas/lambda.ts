1e4a9295c93e9fa3f908f40bc5f76aacd77e804e
Elena Cavallero <elecav@hotmail.it>
2022-09-05 12:41:31 +0200

    add permissions

--- lambdas/lambda.ts
+++ lambdas/lambda.ts
@@ -1,3 +1,20 @@
-export const handler = () => {
-  return { statusCode: 200, body: "Hello world" };
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({ region: "eu-west-1" });
+const input = {
+  TableName: process.env.TABLE_NAME,
+};
+const command = new ScanCommand(input);
+
+export const handler = async () => {
+  try {
+    const data = await client.send(command);
+    console.log("DYNAMODATA", data);
+    // process data.
+  } catch (error) {
+    console.log("ERROR", error);
+    // error handling.
+  }
+
+  //   return { statusCode: 200, body: "Hello world" };
 };
--- lib/birthday-app-email-service-stack.ts
+++ lib/birthday-app-email-service-stack.ts
@@ -1,14 +1,32 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-
+import {
+  Policy,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+  Effect,
+} from "aws-cdk-lib/aws-iam";
 export class BirthdayAppEmailServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, "my-handler", {
+    const emailLambda = new NodejsFunction(this, "my-handler", {
       entry: "./lambdas/lambda.ts",
+      environment: {
+        TABLE_NAME: "User-vnuwshx4qvcddcqx24ru2aywiy-dev",
+      },
     });
+
+    emailLambda.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["ses:SendEmail", "SES:SendRawEmail"],
+        resources: ["*"],
+        effect: Effect.ALLOW,
+      })
+    );
 
     // The code that defines your stack goes here
 
